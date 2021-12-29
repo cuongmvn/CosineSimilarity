50080560240d003736ac1740f8dd67d8c1149539
cuongmvn <cuongmvn@gmail.com>
2021-12-29 16:11:59 +0100

    Test commit

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+import re
+import string
+import numpy as np
+def pre_process(s):
+    res = re.sub('['+string.punctuation+']', '', s.lower()).split()
+    return res
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def make_dictionary(list_of_words):
+    sentence_dict = {}
+    k = 0
+    for word in list_of_words:
+        if word not in sentence_dict:
+            sentence_dict[word] = k
+            k = k+1
+    return sentence_dict
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+def make_vector(dictionary, sentence_words):
+    vector = np.zeros(len(dictionary))
+    for word in sentence_words:
+        vector[dictionary.get(word)] += 1
+    return vector
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    s1 = "I am, happy!!!"
+    s2 = "I am not sad, I really do!"
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    w1 = pre_process(s1)
+    w2 = pre_process(s2)
+
+    total_words = np.concatenate((w1, w2))
+    dictionary = make_dictionary(total_words)
+
+    v1 = make_vector(dictionary, w1)
+    v2 = make_vector(dictionary, w2)
+
+    cosine_similarity = np.dot(v1, v2)/(np.linalg.norm(v1)*np.linalg.norm(v2))
+    print(cosine_similarity)
